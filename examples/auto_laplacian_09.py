0922dfb5fdb839d486e4bd5fe8f3cfc1be7ae3d8
Ivan Damnjanovic <id1226@gmail.com>
2026-01-07 02:27:05 +0100

    Examples added and removed.

--- examples/auto_laplacian_09.py
+++ examples/auto_laplacian_09.py
@@ -29,7 +29,7 @@ def graph_invariant(graph_batch: Graph):
     spectrum_batch = np.linalg.eigvalsh(lap_batch)
     mu_batch = spectrum_batch[:, -1]
 
-    temp = np.max((2.0 * degree_batch_1 ** 2) / and_batch_1, axis=1)
+    temp = np.max(0.5 * and_batch_1 + 1.5 * degree_batch_1, axis=1)
     result = mu_batch - temp
 
     result[spectrum_batch[:, 1] < 0.15] = -1000.0
